97ab260692a54acde0c1f76d29ec05c0d92beb47
asm-ayesha <ayesha219333@gmail.com>
2025-11-23 15:04:44 +0600

    comparison lowercase and uppercase and trim

---
+++ case.js
@@ -0,0 +1,28 @@
+let school ='GHORA-CHORA HIGH SCHOOL'
+console.log(school)
+
+
+//uppercase : ABCD
+//lowercase : abcd
+
+let subject ='Chemistry';
+let book = 'chemistry';
+
+if(subject.toLowerCase() == book.toLowerCase()){
+    console.log('i reading the book')
+}
+
+else{
+    console.log('i not read book')
+}
+
+let drink = 'water'
+let liquid = ' water '
+
+if(drink.trim() === liquid.trim()){
+    console.log('pni e rupor nam jibon')
+}
+else{
+    console.log('sumodre pani ache khaite pari na')
+}
+
---
+++ string.js
@@ -0,0 +1,9 @@
+let country = 'Bangladesh'; 
+let division = `rajshahi`
+let district =  new String('sirajganj')
+
+console.log(typeof(district))
+
+// string is imutable --> not changebale
+console.log(country.length)
+console.log(country[0])
