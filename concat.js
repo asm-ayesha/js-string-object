fbb20f20b78de270c5e8e1aa8f0f432e4d6444d7
asm-ayesha <ayesha219333@gmail.com>
2025-11-23 15:46:22 +0600

    string slice, join,concat, includes

---
+++ concat.js
@@ -0,0 +1,12 @@
+let frist = 'ayesha';
+let last = 'siddika';
+// let fullName = frist + ' '+ last;
+// console.log(fullName)
+
+let fullName = frist.concat(' ').concat(last);
+console.log(fullName)
+
+
+//Includes
+
+console.log(last.includes('g'))
---
+++ slice.js
@@ -0,0 +1,10 @@
+let address = 'bangladesh'
+let part = address.slice(4,10);
+console.log(part)
+
+let text = 'i love js'
+let result =text.split('',3)
+console.log(result)
+
+let friends = ['abul', 'habul', 'gabul', 'dabul', 'bulbul']
+console.log(friends.join())
