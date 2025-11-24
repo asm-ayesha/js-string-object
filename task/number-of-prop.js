0925a63ea334f9dc3251fa4ba9f337a288ffbf11
asm-ayesha <ayesha219333@gmail.com>
2025-11-24 19:44:01 +0600

    object-js-task

---
+++ task/number-of-prop.js
@@ -0,0 +1,13 @@
+// Count the number of properties.
+
+
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+let countProp=Object.keys(student).length
+console.log(countProp)
